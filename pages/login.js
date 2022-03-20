83e46a59735d830787f4170a21b0b1c4040993cd
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2022-03-20 19:35:42 -0400

    fix: fix css modules

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,18 @@
-import Link from 'next/link'
-import '../styles/Login.module.css'
+import {loginBox} from "../styles/Login.module.css";
 
 export default function Login() {
   return (
-    <div className = "loginBox">
+    <div className={loginBox}>
       <h6>Login. Enter your username and password.</h6>
       <form>
-        <label>
-          Username:
-        </label>
-        <input type="text" id="user" name="user" required></input>
-        <br/>
-        <label>
-          Password:
-        </label>
-        <input type="text" id="pw" name="pw"></input>
-        <br/>
-        <input type="submit">Submit</input>
+        <label>Username:</label>
+        <input type="text" id="user" name="user" required />
+        <br />
+        <label>Password:</label>
+        <input type="text" id="pw" name="pw" />
+        <br />
+        <input type="submit" />
       </form>
     </div>
-  )
+  );
 }

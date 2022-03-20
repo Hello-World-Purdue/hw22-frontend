5b73e97190111c3889436cc0c73e8a43cf851d92
Gao Changxiang <gao654@purdue.edu>
2022-03-19 20:39:03 -0400

    created basic login box with textfields and submit button

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,23 @@
 import Link from 'next/link'
+import './login.css'
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login. Enter your username and password.</h1>
+    <div className = "loginBox">
+      <h6>Login. Enter your username and password.</h6>
+      <form>
+        <label for="username">
+          Username:
+        </label>
+        <input type="text" id="user" name="user" required></input>
+        <br/>
+        <label for="password">
+          Password:
+        </label>
+        <input type="text" id="pw" name="pw"></input>
+        <br/>
+        <input type="submit">Submit</input>
+      </form>
     </div>
   )
 }

cf6e4112533b4f3b3937059cfd5ccdd3a5e2b816
PayasBhutra <pbhutra@purdue.edu>
2022-03-27 21:00:16 -0400

    added faq page

---
+++ pages/faq.js
@@ -0,0 +1,131 @@
+import React, { FC, useState } from "react";
+
+export default function Faq() {
+    const people = [{
+        number: 0,
+        question: 'What’s the best ways to find teammates?',
+        answera: 'Ask friends',
+        answerb: 'Team forming events at social (Sept 16) and day-of hackathon', 
+      }, {
+        number: 1,
+        question: 'Why do companies prefer hackathons to career fairs and what’s in it for students?',
+        answera: 'Career fairs don’t give you much chance to talk to the recruiters',
+        answerb: 'Hackathons provide better interactions between students and recruiters',
+        answerc: 'Figure out if the company is recruiting and/or showcasing technologies and ask them questions that align with this purpose',
+      }, {
+        number: 2,
+        question: 'How do you get a competitive edge for the showcase portion?',
+        answera: 'Be confident--presenting yourself as well as project',
+        answerb: 'Be excited about your project',
+        answerc: 'Make sure to prepare for the presentation',
+      }, {
+        number: 3,
+        question: 'Who should I ask when I’m stuck?',
+        answera: 'You can ask:',
+        answeri: 'Other hackers',
+        answerii: 'Mentor/organizers through the HelpQ',
+        answeriii: 'Company recruiters',
+      }, {
+        number: 4,
+        question: 'How does the judging process work?',
+        answera: 'Science fair styled--everyone has table and presents to those roaming around',
+        answerb: 'Judged by recruiters and organizers',
+        answerc: 'Results at closing ceremony',
+      }, {
+        number: 5,
+        question: 'Can I work on projects I’ve already started?',
+        answera: 'NO',
+        answerb: 'However it is encouraged to have thought about your project/looked at APIs beforehand--But don’t code anything!',
+        answerc: 'Projects must be started at the hackathon',
+      }, {
+        number: 6,
+        question: 'What if I can’t install large IDEs on my laptop?',
+        answera: 'Feel free to contact/research organizers/mentors to see if there are options there for you.',
+        answerb: 'Can still build cool projects without big IDEs. Example: vim, notepad, nano, emacs',
+      }, {
+        number: 7,
+        question: 'What prizes do winners get?',
+        answera: '1st: Amazon Echo, 2nd: TOZO Wireless Charge, 3rd: Anker Bluetooth Speaker',
+       
+      }, {
+        number: 8,
+        question: 'Will there be food at the hackathon?',
+        answera: 'Yes. Lunch, dinner, and breakfast will be provided',
+        
+      }, {
+        number: 9,
+        question: 'Where do we submit the projects?',
+        answera: 'Devpost link that will be provided during the day-of hackathons.',
+        answerb: 'Mentors will be available to help submit projects',
+        
+      }, {
+        number: 10,
+        question: 'How much experience do we need to participate in the hackathon?',
+        answera: 'No prior experience needed. Hackathons are a great learning experience for a lot of different tools and languages and if you have any questions, mentors are available to help you through the HelpQ.',
+        
+      }, {
+        number: 11,
+        question: 'Is it alright if we don’t have a group?',
+        answera: 'Yes. You can work alone or create teams of up to 4 during team-building events on the social (Sept 16) and day-of',
+        
+      }, {
+        number: 12,
+        question: 'What is the maximum number of teammates in a team?',
+        answera: 'Up to 4 people per team',
+        
+      }, {
+        number: 13,
+        question: 'I’m on the waitlist, can I still get in?',
+        answera: 'Yes. If you appear on the opening ceremony of the day-of, we can check if there are any available spots',
+        
+      }, {
+        number: 14,
+        question: 'Do I need to be a CS/DS major to participate?',
+        answera: 'No. Freshman from any majors are welcome to participate.',
+        
+      }, {
+        number: 15,
+        question: 'Do I need to stay at the location for the whole 24-hours?',
+        answera: 'No. Just make sure to attend the opening, project demo, and closing.',
+        
+      }, {
+        number: 16,
+        question: 'Can I work somewhere outside of the hackathon location?',
+        answera: 'Yes, but only if it is agreed upon by your whole team. However, only participants working at the hackathon location can get access to recruiters and mentors.',
+      }, {
+        number: 17,
+        question: 'When will I be notified of the application results?',
+        answera: 'September 11 through your email',
+      }, {
+        number: 18,
+        question: 'When is the deadline to RSVP after getting the application results back?',
+        answera: 'September 13',
+      },
+    
+    ];
+      const listItems = people.map(person =>
+        <li key={person.number}>
+          <p>
+            <b>{person.question}</b>
+          </p>
+          <p>
+          <ol>{person.answera}</ol>
+          <ul>{person.answeri}</ul>
+          <ul>{person.answerii}</ul>
+          <ul>{person.answeriii}</ul>
+          <ol>{person.answerb}</ol>
+          <ol>{person.answerc}</ol>
+          
+          </p>
+        </li>
+      );
+      
+        
+return (
+<body>
+<h2>FAQ</h2>
+<ul>{listItems}</ul>
+    
+    </body>
+)
+}

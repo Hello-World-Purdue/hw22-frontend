cc03f663404de6e9c95bce72ea38ec47b7c50d7d
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2022-04-26 11:52:33 -0400

    Merge branch 'main' of https://github.com/Hello-World-Purdue/hw22-frontend into main

---
+++ context/AlertContext.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const initialState = {
+    title: null, // What the error corresponds to (Login error, Application error)
+    message: null, // The error message to display
+    type: null // error or success
+}
+
+const AlertContext = React.createContext({
+    ...initialState,
+    setAlert: (type, title, message) => {},
+    clearAlert: () => {}
+});
+
+export const AlertContextProvider = (props) => {
+    const [state, setState] = useState(initialState);
+
+    // Set error
+    const setAlert = (type, title, message) => {
+        setState({
+            type,
+            title,
+            message
+        });
+    }
+
+    // Clear error
+    const clearAlert = () => {
+        setState({
+            type: null,
+            title: null,
+            message: null
+        })
+    }
+
+    return (
+        <AlertContext.Provider value={{
+            type: state.type,
+            title: state.title,
+            message: state.message,
+            setAlert,
+            clearAlert
+        }}>
+            {props.children}
+        </AlertContext.Provider>
+    )
+}
+
+export default AlertContext;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import "../styles/globals.css";
 import { Navbar } from "../components/navbar";
 
+// Context providers
+import AlertContext from "../context/AlertContext";
+import UserContext from "../context/UserContext";
+import AuthContext from "../context/AuthContext";
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Navbar />
-      <Component {...pageProps} />
-    </div>
-  );
+	return (
+		<div>
+			<AlertContext>
+				<AuthContext>
+					<UserContext>
+						<Navbar />
+						<Component {...pageProps} />
+					</UserContext>
+				</AuthContext>
+			</AlertContext>
+		</div>
+	);
 }
 
 export default MyApp;

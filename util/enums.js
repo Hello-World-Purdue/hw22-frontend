022d912770b3b882af49ee61e552845a013ce746
Anushka N <anushkasn17@gmail.com>
2022-08-25 17:00:13 -0400

    Added Application form

---
+++ util/enums.js
@@ -0,0 +1,71 @@
+export const Gender = {
+    MALE: "Male",
+    FEMALE: "Female",
+    OTHER: "Other",
+}
+
+export const ethnicities = [
+    "Non-Hispanic White",
+    "Black or African American",
+    "Latino, Latina, or Hispanic American",
+    "South Asian or Indian American",
+    "East Asian or Asian American",
+    "Middle Eastern or Arab American",
+    "Native American or Alaskan Native",
+    "Hawaiian or Pacific Islander",
+    "Multiple ethnicity / Other",
+    "Prefer not to answer",
+];
+
+export const ClassYear = {
+    FRESHMAN: "Freshman",
+    SOPHOMORE: "Sophomore",
+    JUNIOR: "Junior",
+    SENIOR: "Senior",
+}
+
+export const gradYears = [2019, 2020, 2021, 2022, 2023, 2024, 2025];
+
+export const Referral = {
+    SOCIAL_MEDIA: "Social Media",
+    WEBSITE: "Purdue Hackers Website",
+    FLYERS: "Flyers",
+    CLASS: "Class Callout",
+    FRIEND: "Friend",
+    OTHER: "Other",
+}
+
+export const ShirtSize = {
+    SMALL: "S",
+    MEDIUM: "M",
+    LARGE: "L",
+    EXTRA_LARGE: "XL",
+    EXTRA_EXTRA_LARGE: "XXL",
+}
+
+export const Status = {
+    PENDING: "Pending",
+    ACCEPTED: "Accepted",
+    REJECTED: "Rejected",
+    WAITLIST: "Waitlist",
+}
+
+export const Major = {
+    COMPUTER_SCIENCE: "Computer Science",
+    DATA_SCIENCE: "Data Science",
+    COMPUTER_ENGINEERING: "Computer Engineering",
+    ELECTRICAL_ENGINEERING: "Electrical Engineering",
+    MECHANICAL_ENGINEERING: "Mechanical Engineering",
+    ECONOMICS: "Economics",
+    BUSINESS: "Business",
+    SCIENCES: "Sciences",
+    LIBERAL_ARTS: "Liberal Arts",
+    FINE_ARTS: "Fine Arts",
+    UNDECIDED: "Undecided",
+    OTHER: "Other",
+}
+
+export const Resume = {
+    YES: "Yes",
+    NO: "No",
+}

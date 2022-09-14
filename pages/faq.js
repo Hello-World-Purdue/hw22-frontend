988667bc7429b249b07e3ad03a68405484e394cc
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2022-09-14 19:45:27 -0400

    added faq page styles

--- pages/faq.js
+++ pages/faq.js
@@ -1,32 +1,36 @@
 import React from "react";
 import Header from "../components/header";
+import Image from "next/image";
+
 import { questions } from "../util/questions";
-import styles from "../styles/Login.module.css";
+import styles from "../styles/Home.module.css";
 
 export default function Faq() {
 	let idx = 0;
 	const listItems = questions.map((question) => (
-		<div key={idx++}>
-			<div className="styles.faqQuestion">{question.q}</div>
-			
-			<ul>
-				{	
-					question.a.map((ans) => (
-					<li key={idx++}>{ans}</li>
-				))}
-			</ul>
+		<div key={idx++} className={styles.faq}>
+			<div className={styles.faqQuestionContainer}>
+				<div className={styles.faqQuestion}>{question.q}</div>
+			</div>
+
+			<div className={styles.faqAnswerContainer}>
+				<ul>
+					{question.a.map((ans) => (
+						<li className={styles.faqAnswer} key={idx++}>
+							{ans}
+						</li>
+					))}
+				</ul>
+			</div>
+
+			<Image src="/design_black.png" height={50} width={180} alt="gradient" />
 		</div>
 	));
 
 	return (
 		<div>
 			<Header title="FAQ" />
-			{listItems}
-			
-			<h3>Further questions?</h3>
-			<p>
-				Email us at: <strong>helloworldpurdue@gmail.com</strong>
-			</p>
+			<div className={styles.faqList}>{listItems}</div>
 		</div>
 	);
 }
